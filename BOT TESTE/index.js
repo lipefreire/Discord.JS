c021180cfd8c969055c538b1d718804c571e8aad
lipefreire <felipef12123@gmail.com>
2022-07-20 08:59:34 -0300

    att

--- BOT TESTE/index.js
+++ BOT TESTE/index.js
@@ -3,4 +3,14 @@ const client = new Discord.Client({intents: 32767});
 const config = require('./config.json');
 const fs = require('fs');
 
+client.on('ready', () => {
+    console.log("BOT Online!")
+})
+
+client.on('message', (message) => {
+    if(message.content == "ping") {
+        message.channel.send("Pong!")
+    }
+})
+
 client.login(config.token)
